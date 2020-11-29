c54d107979418530b133cf7634e17d3e5e03b614
LucasAparecidoFlorentino <lucasflorentino_2000@hotmail.com>
2020-11-29 18:26:26 -0300

    Feito tela para logout do usuario, começado tela com as informações do barbeiro

--- src/Api.js
+++ src/Api.js
@@ -39,6 +39,19 @@ export default {
         const json = await req.json();
         return json;
     },
+    logout: async () => {
+        const token = await AsyncStorage.getItem('token');
+        const req = await fetch(`${BASE_API}/auth/logout`, {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ token })
+        });
+        const json = await req.json();
+        return json;
+    },
     getBarbers: async (lat = null, lng = null, address= null ) => {
         const token = await AsyncStorage.getItem('token');
 
@@ -51,5 +64,12 @@ export default {
         const json = await req.json();
 
         return json;
+    },
+    getBarber: async (id) => {
+        const token = await AsyncStorage.getItem('token');
+        const req = await fetch(`${BASE_API}/barber/${id}?token=${token}`);
+        const json = await req.json();
+        return json;
+
     }
 };
---
+++ src/screens/Barber/index.js
@@ -0,0 +1,32 @@
+import React, {useState, useEffect} from 'react';
+import { Text } from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native'
+import { Container } from './styles';
+
+import Api from "../../Api";
+
+export default () => {
+    const navigation = useNavigation();
+    const route = useRoute();
+
+    const [userInfo, setUserInfo] = useState({
+        id: route.params.id,
+        avatar: route.params.avatar,
+        name: route.params.name,
+        stars: route.params.stars
+    });
+
+    useEffect(()=>{
+        const getBarberInfo = async () => {
+            let json = await Api.getBarber(userInfo.id);
+        }
+        getBarberInfo();
+
+    }, []);
+
+    return (
+        <Container>
+            <Text>Barbeiro: {userInfo.name}</Text>
+        </Container>
+    );
+}
