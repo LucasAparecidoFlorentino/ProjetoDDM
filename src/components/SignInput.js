06107ba4cef3ead0d0a8029c363a9eba39c01056
Maycon Willian <mayconwas2018@gmail.com>
2020-11-30 20:54:47 -0300

    Criaçãdo do modal de agendamento de serviço e finalização do atendimento.

--- src/components/SignInput.js
+++ src/components/SignInput.js
@@ -4,28 +4,27 @@ import styled from 'styled-components/native';
 const InputArea = styled.View`
     width: 100%;
     height: 60px;
-    background-color: #1E90FF;
+    background-color: #83D6E3;
     flex-direction: row;
     border-radius: 30px;
     padding-left: 15px;
     align-items: center;
     margin-bottom: 15px;
 `;
-
 const Input = styled.TextInput`
     flex: 1;
     font-size: 16px;
-    color: #4169E1;
+    color: #268596;
     margin-left: 10px;
 `;
 
 export default ({IconSvg, placeholder, value, onChangeText, password}) => {
     return (
         <InputArea>
-            <IconSvg width="24" height="24" fill="#4169E1" />
-            <Input 
+            <IconSvg width="24" height="24" fill="#268596" />
+            <Input
                 placeholder={placeholder}
-                placeholderTextColor="#4169E1"
+                placeholderTextColor="#268596"
                 value={value}
                 onChangeText={onChangeText}
                 secureTextEntry={password}
